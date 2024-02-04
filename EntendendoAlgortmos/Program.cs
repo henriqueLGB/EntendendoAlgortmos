6b8be7ec85f6e9e928209cd1c6769ad23a6f7214
Henrique Lira <henriquelirabilla@outlook.com.br>
2024-02-04 01:05:24 -0300

    Adicionar arquivos de projeto.

---
+++ EntendendoAlgortmos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesquisaBinaria
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //LISTA NÚMERICA
+            List<int> listaNumerica = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+            //RECEBENDO O RESULTADO DA PESQUISA
+            int? resultado = PesquisaBinaria(listaNumerica, 8);
+
+            //IMPRIMINDO
+            Console.WriteLine(resultado);
+        }
+
+        public static int? PesquisaBinaria(List<int> lista, int item)
+        {
+            var baixo = 0;
+            var alto = lista.Count() - 1;
+
+            while(baixo <= alto)
+            {
+                var meio  = (baixo + alto) / 2;
+                var chute = lista[meio];
+
+                if (chute == item) return meio;
+
+                if (chute > item)
+                {
+                    alto = meio - 1;
+                }
+                else
+                {
+                    baixo = meio + 1;
+                }
+
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ OrdenacaoSelecao/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace OrdenacaoSelecao
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> arr = new List<int> { 64, 25, 12, 22, 11 };
+  
+            foreach(int num in OrdenacaoPorSelecao(arr))
+            {
+                Console.Write(num + " ");
+            }
+
+        }
+
+        private static int BuscaMenor(List<int> arr)
+        {
+            int menor = arr[0];
+            int menorIndice = 0;
+
+            for (int i = 1; i < arr.Count; i++)
+            {
+                if (arr[i] < menor)
+                {
+                    menor = arr[i];
+                    menorIndice = i;
+                }
+            }
+
+            return menorIndice;
+
+        }
+
+        private static List<int> OrdenacaoPorSelecao(List<int> arr)
+        {
+            List<int> novoArr = new List<int>();
+
+            while (arr.Count > 0)
+            {
+                int menor = BuscaMenor(arr);
+                novoArr.Add(arr[menor]);
+                arr.RemoveAt(menor);
+            }
+
+            return novoArr;
+        }
+    }
+}
